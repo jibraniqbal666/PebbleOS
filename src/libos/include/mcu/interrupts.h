941e0e45fda390e85d8669fc42edb537ad732f6e
jibraniqbal666 <jibran.iqbal@protonmail.com>
2025-11-23 19:11:58 +0100

    progress 744/1460

--- src/libos/include/mcu/interrupts.h
+++ src/libos/include/mcu/interrupts.h
@@ -26,6 +26,12 @@ inline static bool mcu_state_is_isr(void);
 //! priority. Anything below 0xB should not execute any FreeRTOS calls.
 inline static uint32_t mcu_state_get_isr_priority(void);
 
+//! Disable interrupts globally (equivalent to ARM's __disable_irq())
+inline static void mcu_state_disable_interrupts(void);
+
+//! Enable interrupts globally (equivalent to ARM's __enable_irq())
+inline static void mcu_state_enable_interrupts(void);
+
 bool mcu_state_are_interrupts_enabled(void);
 
 #ifdef __arm__
--- src/libos/include/mcu/interrupts_stubs.inl.h
+++ src/libos/include/mcu/interrupts_stubs.inl.h
@@ -14,6 +14,37 @@
  * limitations under the License.
  */
 
+#if defined(MICRO_FAMILY_ESP32C3)
+// ESP32-C3 (RISC-V) interrupt control
+#include "riscv/csr.h"
+#include "riscv/encoding.h"  // For MSTATUS_MIE
+
+static inline void mcu_state_disable_interrupts(void) {
+  // Disable interrupts globally using RISC-V CSR
+  // Clear mstatus.MIE (Machine Interrupt Enable) bit (bit 3 = 0x8)
+  // This is equivalent to ARM's __disable_irq()
+  RV_CLEAR_CSR(mstatus, MSTATUS_MIE);
+}
+
+static inline void mcu_state_enable_interrupts(void) {
+  // Enable interrupts globally using RISC-V CSR
+  // Set mstatus.MIE (Machine Interrupt Enable) bit (bit 3 = 0x8)
+  // This is equivalent to ARM's __enable_irq()
+  RV_SET_CSR(mstatus, MSTATUS_MIE);
+}
+#else
+// Generic non-ARM implementation (stub)
+static inline void mcu_state_disable_interrupts(void) {
+  // Stub implementation for non-ARM platforms
+  // This should be implemented per platform if needed
+}
+
+static inline void mcu_state_enable_interrupts(void) {
+  // Stub implementation for non-ARM platforms
+  // This should be implemented per platform if needed
+}
+#endif
+
 static inline bool mcu_state_is_isr(void) {
   return false;
 }
