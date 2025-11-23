b809527122c63ad3b5d15d93e3672b7a98de9d83
jibraniqbal666 <jibran.iqbal@protonmail.com>
2025-11-23 18:05:47 +0100

    progress 704/1460

--- src/fw/drivers/stubs/mcu.c
+++ src/fw/drivers/stubs/mcu.c
@@ -8,5 +8,11 @@ StatusCode mcu_get_serial(void *buf, size_t *buf_sz) {
 }
 
 uint32_t mcu_cycles_to_milliseconds(uint64_t cpu_ticks) {
+#if defined(MICRO_FAMILY_ESP32C3)
+  // ESP32-C3 default CPU frequency is 160MHz (from sdkconfig.h)
+  const uint32_t ESP32C3_CPU_FREQ_HZ = 160000000UL;
+  return ((cpu_ticks * 1000) / ESP32C3_CPU_FREQ_HZ);
+#else
   return ((cpu_ticks * 1000) / SystemCoreClock);
+#endif
 }
